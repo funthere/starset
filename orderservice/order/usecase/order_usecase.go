1d1c846d5c165ff35a7ab94edd21bf7cd14a27a7
Suvander <suvander@gmail.com>
2023-11-06 19:42:45 +0700

    order service implement order usecase

---
+++ orderservice/order/usecase/order_usecase.go
@@ -0,0 +1,29 @@
+package usecase
+
+import (
+	"context"
+
+	"github.com/funthere/starset/orderservice/domain"
+)
+
+type orderUsecase struct {
+	orderRepo domain.OrderRepository
+}
+
+func NewOrderUsecase(repo domain.OrderRepository) domain.OrderUsecase {
+	return &orderUsecase{
+		orderRepo: repo,
+	}
+}
+
+func (u orderUsecase) Store(ctx context.Context, order *domain.Order) error {
+	return u.orderRepo.Store(ctx, order)
+}
+
+func (u orderUsecase) Fetch(ctx context.Context, filter domain.Filter) ([]domain.Order, error) {
+	return u.orderRepo.Fetch(ctx, filter)
+}
+
+func (u orderUsecase) PatchStatus(ctx context.Context, id int64, status int) error {
+	return u.PatchStatus(ctx, id, status)
+}
